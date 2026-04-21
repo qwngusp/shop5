7cb55f846c309a69cb8f47a11ded649aa846514a
qwngusp <qwngusp@gmail.com>
2026-04-22 00:48:47 +0900

    4/21 피드백 내용 반영

--- js/logger.js
+++ js/logger.js
@@ -2,7 +2,7 @@
 // Google Apps Script 웹앱으로 로그 전송
 
 const Logger = (() => {
-  const ENDPOINT = 'https://script.google.com/macros/s/AKfycbwHDEzxuSua94pgZ9-wLrJW5sZXb1iazIC2KI2LA2GbXLVojcbCtnBtqtydZvHZRCTr/exec';
+  const ENDPOINT = 'https://script.google.com/macros/s/AKfycbyww_xHvcUgXpa3viioEJFO7EZ-h2YkdCiUsPrMxYfaejdTIDlOnNlbRYfmnbTnS5w/exec';
 
   const send = async (type, payload) => {
     try {
